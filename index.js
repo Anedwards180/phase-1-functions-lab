0b057816c49bce6ab7de033a51722a1bac9ab1d8
Alex Edwards <anedwards180@gmail.com>
2021-08-10 22:13:12 -0500

    commit tree c5048c0e8a9551c2ca5e9f0d7b259346bfe91eac

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Code your solution in this file!
+const hq = 42
+let distance;
+let feet
+function distanceFromHqInBlocks(startingBlock) {
+    if (startingBlock < hq){
+        distance = (hq - startingBlock)
+        
+    }
+
+    else if(startingBlock >= hq) {
+        distance = (startingBlock - hq)
+        
+    }
+
+    return distance
+
+}
+
+function distanceFromHqInFeet(startingBlock) {
+    return distanceFromHqInBlocks(startingBlock)*264;
+
+
+    }
+
+
+
+function distanceTravelledInFeet(start, end) {
+    if (start >= end) {
+      return (start - end)* 264;   
+    }
+
+    else if (end > start) {
+        return (end - start)*264;
+    }
+
+
+}
+
+function calculatesFarePrice(start, end) {
+   let distance = distanceTravelledInFeet (start, end)
+
+
+    if (distance <= 400)
+      return 0;
+
+    else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+    }
+
+    else if (distance > 2000 && distance < 2500) {
+        return 25
+    }
+
+    else {
+        return 'cannot travel that far'
+    }
+}
